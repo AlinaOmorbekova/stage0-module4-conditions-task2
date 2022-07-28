731013a7b2ec60a173a197b556bd8a299ece5b77
AlinaOmorbekova <alina.omorbekova@iaau.edu.kg>
2022-07-28 21:10:19 +0600

    task is done

--- src/main/java/school/mjc/stage0/conditions/task2/CoinFlip.java
+++ src/main/java/school/mjc/stage0/conditions/task2/CoinFlip.java
@@ -2,6 +2,14 @@ package school.mjc.stage0.conditions.task2;
 
 public class CoinFlip {
     public void throwCoin(int from1UpTo1000) {
+        boolean isEagle = from1UpTo1000 < 500;
+        boolean isTail = from1UpTo1000 > 500;
+        if (isEagle){
+            System.out.println("Eagle");
+        }
+        else{
+            System.out.println("Tail");
+        }
 
     }
 }
